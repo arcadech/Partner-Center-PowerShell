d708457fb8828e7bfdadd76d7d0d76f40d785ecd
Claudio Spizzi <claudio.spizzi@arcade.ch>
2023-01-24 20:20:26 +0100

    Update publish script

--- publish.ps1
+++ publish.ps1
@@ -27,11 +27,11 @@ if (Test-Path -Path $modulePath)
     throw "The module PartnerCenter with version $Version already exists."
 }
 
-$libraryPath = '{0}\lib' -f $PSScriptRoot
-if (-not (Test-Path -Path $libraryPath))
-{
-    throw "The Release Build Path \lib does not exist."
-}
+# $libraryPath = '{0}\lib' -f $PSScriptRoot
+# if (-not (Test-Path -Path $libraryPath))
+# {
+#     throw "The Release Build Path \lib does not exist."
+# }
 
 $artifactPath = '{0}\artifacts\Release' -f $PSScriptRoot
 if (-not (Test-Path -Path $artifactPath))
@@ -41,13 +41,13 @@ if (-not (Test-Path -Path $artifactPath))
 
 New-Item -Path "$modulePath\PreloadAssemblies" -ItemType 'Directory' | Out-Null
 Copy-Item -Path "$artifactPath\PreloadAssemblies\*" -Destination "$modulePath\PreloadAssemblies"
-Copy-Item -Path "$libraryPath\Microsoft.Identity.Client\4.23.0.0\Microsoft.Identity.Client.dll" -Destination "$modulePath\PreloadAssemblies"
-Copy-Item -Path "$libraryPath\Microsoft.Identity.Client.Extensions.Msal\2.16.6.0\Microsoft.Identity.Client.Extensions.Msal.dll" -Destination "$modulePath\PreloadAssemblies"
+# Copy-Item -Path "$libraryPath\Microsoft.Identity.Client\4.23.0.0\Microsoft.Identity.Client.dll" -Destination "$modulePath\PreloadAssemblies"
+# Copy-Item -Path "$libraryPath\Microsoft.Identity.Client.Extensions.Msal\2.16.6.0\Microsoft.Identity.Client.Extensions.Msal.dll" -Destination "$modulePath\PreloadAssemblies"
 Copy-Item -Path "$artifactPath\Microsoft.Extensions.*" -Destination $modulePath
 Copy-Item -Path "$artifactPath\Microsoft.Graph.*" -Destination $modulePath
 Copy-Item -Path "$artifactPath\Microsoft.Identity.*" -Destination $modulePath
 Copy-Item -Path "$artifactPath\Microsoft.IdentityModel.*" -Destination $modulePath
-Copy-Item -Path "$artifactPath\Microsoft.Rest.*" -Destination $modulePath
+# Copy-Item -Path "$artifactPath\Microsoft.Rest.*" -Destination $modulePath
 Copy-Item -Path "$artifactPath\Microsoft.Store.PartnerCenter.*" -Destination $modulePath
 Copy-Item -Path "$artifactPath\newtonsoft.Json.Dll" -Destination $modulePath
 Copy-Item -Path "$artifactPath\PartnerCenter.ps*1" -Destination $modulePath
@@ -56,4 +56,4 @@ Copy-Item -Path "$artifactPath\System.Runtime.CompilerServices.Unsafe.dll" -Dest
 Copy-Item -Path "$artifactPath\System.Security.Cryptography.ProtectedData.dll" -Destination $modulePath
 
 $token = Use-VaultSecureString -TargetName 'PowerShell Gallery Key (arcade)' | Unprotect-SecureString
-Publish-Module -Path $modulePath -Repository 'Arcade' -NuGetApiKey $token -Confirm:$true -Verbose
+# Publish-Module -Path $modulePath -Repository 'Arcade' -NuGetApiKey $token -Confirm:$true -Verbose
